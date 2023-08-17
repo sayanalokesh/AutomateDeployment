95df5b5285fab35a150f3626e6af9bc3531b18f5
sayanalokesh <lokesh.sayana@gmail.com>
2023-08-18 00:45:47 +0530

    add new python program file and also config.py file

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# GitHub repository details
+access_token = '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'
+owner = 'sayanalokesh'
+repo_name = 'AutomateDeployment'
+branch = 'dev'
+
+# Paths
+local_repo_path = r"C:\Users\Lokesh\OneDrive\Desktop\Hero Vired\Sessions\AutomateDeployment"
+nginx_path = r"C:\nginx-1.24.0\autodeploy"
+file_to_copy = "index.html"
